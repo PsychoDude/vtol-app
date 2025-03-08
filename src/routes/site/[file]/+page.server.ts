ed67a4141981917a5389dd95b96c3356ace067a1
Marc Lipari <marc.lipari@gmail.com>
2025-03-08 18:16:46 -0500

    added site pages

---
+++ src/routes/site/[file]/+page.server.ts
@@ -0,0 +1,21 @@
+import { error } from '@sveltejs/kit';
+import { marked } from 'marked';
+
+export const prerender = true;
+
+export async function load({ params, fetch }) {
+	const response = await fetch(`/checklists/site/${params.file}.md`);
+
+	if (response.status !== 200) return error(404, 'LOSER');
+
+	const markdownContent = await response.text();
+	let html = await marked(markdownContent);
+
+	html = html.replace(/<img([^>]*?)>/g, (match, attributes) => {
+		return `<img ${attributes} class="w-full h-auto" />`;
+	});
+
+	return {
+		content: html
+	};
+}
---
+++ src/routes/site/[file]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Checklist from '$lib/components/Checklist.svelte';
+
+	export let data;
+</script>
+
+<Checklist {data} />
