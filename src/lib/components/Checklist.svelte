f45070b8d2bc2d8be89dd205c9ab3e3c3144d9f1
Marc Lipari <marc.lipari@gmail.com>
2025-03-08 14:30:20 -0500

    removed unused code  fromChecklist component

--- src/lib/components/Checklist.svelte
+++ src/lib/components/Checklist.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
 	let { aircraft, file, markdown } = $props();
-
-	if (typeof window !== 'undefined') {
-		// stash the value...
-		const initial = markdown;
-
-		// unset it...
-		markdown = undefined;
-
-		$effect(() => {
-			// ...and reset after we've mounted
-			markdown = initial;
-		});
-	}
 </script>
 
 {@html markdown}
