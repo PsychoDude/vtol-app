0807163e1a6a207ea99306c6807750d46e598bb5
Marc Lipari <marc.lipari@gmail.com>
2025-03-08 14:30:45 -0500

    Main page looks good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+
+	let { data } = $props();
+</script>
+
+<div class="flex space-x-4">
+	<div class="mx-auto grid grow grid-cols-3 gap-4">
+		{#each data.aircraft as plane}
+			<Button info={plane} color="blue" mainBtns="true" />
+		{/each}
+
+		{#each data.globalPages as page}
+			<Button info={page} color="green" mainBtns="true" />
+		{/each}
+	</div>
+</div>
