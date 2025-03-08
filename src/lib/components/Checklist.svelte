0e2e31a0d9bbcb441e26928c1071ce5e9e6f6163
Marc Lipari <marc.lipari@gmail.com>
2025-03-08 18:04:28 -0500

    fixed checklist component props

--- src/lib/components/Checklist.svelte
+++ src/lib/components/Checklist.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 
-	let { markdown } = $props();
+	let { data } = $props();
+
 	const backBtnName: { name: string } = { name: 'Back' };
 </script>
 
 <Button type="back" info={backBtnName} mainBtns="false" />
 
 <div class="my-4">
-	{@html markdown}
+	{@html data.content}
 </div>
