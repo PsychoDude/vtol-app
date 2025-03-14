fc7286ddb7bf9db0d554603b1038d2ac5cff5f28
Marc Lipari <marc.lipari@gmail.com>
2025-03-14 16:57:43 -0400

    you win

--- src/routes/[...path]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from '../[aircraft]/$types';
-
-export const prerender = false;
-
-export const load: PageServerLoad = async () => {
-	return error(404, 'Not Found.');
-};
