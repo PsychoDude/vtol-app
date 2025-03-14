6489fa6bb25accadd59199ccfafa4c8837e3ed65
Marc Lipari <marc.lipari@gmail.com>
2025-03-14 16:28:08 -0400

    let's test this

--- src/routes/[aircraft]/[...path]/+page.ts
+++ src/routes/[aircraft]/[...path]/+page.ts
@@ -1,6 +1,9 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+export const ssr = false;
+
 export const load: PageLoad = () => {
 	error(404, 'Page Not Found.');
 };
