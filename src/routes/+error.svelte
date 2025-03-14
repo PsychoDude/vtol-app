f18244bc5e9d738f2ce7fc82fca67a220a926441
Marc Lipari <marc.lipari@gmail.com>
2025-03-14 17:19:29 -0400

    dang it

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import Button from '$lib/components/Button.svelte';
-	import { onMount } from 'svelte';
-
-	let error: App.Error | null;
-
-	onMount(() => {
-		error = page.error;
-	});
 </script>
 
 <div class="flex h-[100vh] flex-col items-center gap-5">
@@ -17,7 +10,7 @@
 		<Button type="home" name="Home" />
 	</div>
 
-	{#if error}
-		<h1 class="text-center text-5xl text-white">{error.message}</h1>
+	{#if page.error}
+		<h1 class="text-center text-5xl text-white">{page.error.message}</h1>
 	{/if}
 </div>
