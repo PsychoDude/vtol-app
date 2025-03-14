9d7fee85f7661dc9e071acc88748c6184de9a480
Marc Lipari <marc.lipari@gmail.com>
2025-03-14 18:17:35 -0400

    test

--- svelte.config.js
+++ svelte.config.js
@@ -9,15 +9,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
 	generateSitemap: false,
 	extensions: ['.svelte', '.svx']
