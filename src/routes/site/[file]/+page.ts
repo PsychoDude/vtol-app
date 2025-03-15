d0dff2c6ef6688a693f8ecd38d7b77ebc9cbc27c
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 14:57:02 -0400

    logic

--- src/routes/site/[file]/+page.ts
+++ src/routes/site/[file]/+page.ts
@@ -1,5 +1,7 @@
+import { getSiteSlugs } from '$lib/markdown';
 import { error } from '@sveltejs/kit';
 
-export const load = () => {
-	error(404, 'Page Not Found.');
+export const load = ({ params }) => {
+	const slugs = getSiteSlugs();
+	if (!slugs.find((file) => file.file === params.file)) error(404, 'Page Not Found.');
 };
