5e672865f71e4e83750e1388b64eac2e12930e8e
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 19:24:24 -0400

    this?

--- svelte.config.js
+++ svelte.config.js
@@ -17,10 +17,7 @@ const config = {
 			fallback: '404.html',
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			relative: false
-		}
+		})
 	},
 	generateSitemap: false,
 	extensions: ['.svelte', '.svx']
