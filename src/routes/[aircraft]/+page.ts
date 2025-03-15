d18c53a8ef4d1c79226e8783b2d2b8b12e0b331c
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 15:04:05 -0400

    test

--- src/routes/[aircraft]/+page.ts
+++ src/routes/[aircraft]/+page.ts
@@ -2,6 +2,8 @@ import { getAircraftOnlySlugs } from '$lib/markdown';
 import { error } from '@sveltejs/kit';
 
 export const load = ({ params }) => {
+	console.log('fired');
+
 	const slugs = getAircraftOnlySlugs();
 	if (!slugs.find((aircraft) => aircraft.aircraft === params.aircraft))
 		error(404, 'Page Not Found.');
