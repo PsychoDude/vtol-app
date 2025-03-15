947f478fe4497496a605c115e48d226a44ab2513
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 13:50:52 -0400

    :p

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,16 @@
 
 				<h1 class="text-center text-5xl text-white">{page.error!.message}</h1>
 			</div>
+		{:else if page.error}
+			<div class="flex flex-col items-center gap-5">
+				<div class="flex w-100 justify-start gap-3">
+					<Button type="back" name="Back" />
+
+					<Button type="home" name="Home" />
+				</div>
+
+				<h1 class="text-center text-5xl text-white">{page.error!.message}</h1>
+			</div>
 		{/if}
 		{@render children?.()}
 	</div>
