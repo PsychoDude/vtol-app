a245cd71fb9b775f845526e6a5e640ca118a7280
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 13:55:26 -0400

    a

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import Button from '$lib/components/Button.svelte';
 
 	let { children } = $props();
+
+	console.log(page.status);
 </script>
 
 <main class="container mx-auto p-4">
@@ -16,9 +18,9 @@
 					<Button type="home" name="Home" />
 				</div>
 
-				<h1 class="text-center text-5xl text-white">{page.error!.message}</h1>
+				<h1 class="text-center text-5xl text-white">Page Not Found!</h1>
 			</div>
-		{:else if page.error}
+		{:else if page.status !== 200}
 			<div class="flex flex-col items-center gap-5">
 				<div class="flex w-100 justify-start gap-3">
 					<Button type="back" name="Back" />
@@ -26,7 +28,7 @@
 					<Button type="home" name="Home" />
 				</div>
 
-				<h1 class="text-center text-5xl text-white">{page.error!.message}</h1>
+				<h1 class="text-center text-5xl text-white">Page Not Found</h1>
 			</div>
 		{/if}
 		{@render children?.()}
