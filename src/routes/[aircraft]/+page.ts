2a3809d71a6a5536fe71b734552828359adb5ddf
Marc Lipari <marc.lipari@gmail.com>
2025-03-15 21:05:00 -0400

    page.ts files were breaking site reload

--- src/routes/[aircraft]/+page.ts
+++
@@ -1,7 +0,0 @@
-import { getAircraftOnlySlugs } from '$lib/markdown';
-import { error } from '@sveltejs/kit';
-
-export const load = ({ params }) => {
-	const aircraft = getAircraftOnlySlugs();
-	if (!aircraft.some((plane) => plane.aircraft === params.aircraft)) throw error(404, ' Poo');
-};
--- src/routes/[aircraft]/case-1/[file]/+page.ts
+++
@@ -1,7 +0,0 @@
-import { getAircraftSlugs } from '$lib/markdown';
-import { error } from '@sveltejs/kit';
-
-export const load = ({ params }) => {
-	const aircraft = getAircraftSlugs();
-	if (!aircraft[1].find((slug) => slug.file === params.file)) throw error(404, ' Poo');
-};
