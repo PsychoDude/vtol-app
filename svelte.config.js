40939148364104e050ac931c44cfea3a6bd80949
Marc Lipari <marc.lipari@gmail.com>
2025-04-03 11:20:02 -0400

    updated deps and changed to static adapt to test

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,9 +9,16 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx'] })],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	},
-	generateSitemap: true,
 	extensions: ['.svelte', '.svx']
 };
 
