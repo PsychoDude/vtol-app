300543af552bb14f3d116f16e2c0a76011d89b21
Marc Lipari <marc.lipari@gmail.com>
2025-04-03 11:23:49 -0400

    try to prerender

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import { siteChecklistStruct, getAllAircraftNames } from '$lib/checklists';
 import type { LayoutServerLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutServerLoad = () => {
 	const sitePages: Array<{ name: string; file: string }> = [];
 	const aircraftNames = getAllAircraftNames();
